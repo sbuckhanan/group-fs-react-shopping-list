fabbf8358508a09729bf92c2051bd689a39b8303
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-07-07 14:06:57 -0500

    finished wireframing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,80 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
+
+
 function App() {
+    const [itemList, setItemList] = useState([])
+    const [newItem, setNewItem] = useState('')
+    const [newQuantity, setNewQuantity] = useState('')
+    const [newUnit, setNewUnit] = useState('')
+
+
+    // useEffect(() => {
+    //     getItems()
+    // }, [])
+
+    // const getItems = () => {
+    //     axios.get('/list')
+    //     .then(response => {
+    //         setItemList(response.data)
+    //     })
+    //     .catch(err => {
+    //         alert('error getting items');
+    //         console.log(err);
+    //     })
+    // }
+
+
+
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <h2>Add an Item</h2>
+
+
+                <form>
+        <label>Item:</label>
+        <input 
+        onChange={ (event) => setNewItem(event.target.value) } 
+        value={newItem}
+        />
+        <label>Quantity:</label>
+        <input 
+        onChange={ (event) => setNewQuantity(event.target.value) } 
+        value={newQuantity}/>
+        <label>Unit:</label>
+        <input 
+        onChange={ (event) => setNewUnit(event.target.value) } 
+        value={newUnit}/>
+        <button type="submit">Save</button>
+    </form>
+
+    <h2>Item List</h2>
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Unit</th>
+        </tr>
+        </thead>
+        <tbody>
+                <td>Apples</td>
+                <td>5</td>
+                <td>2lbs</td>
+                <button type="delete">Remove</button>
+                <button type="Edit">Buy</button>
+        </tbody>
+    </table>
+
             </main>
         </div>
+        
     );
 }
 
