c9b4171f54322ae065d48b52a6d3d313f0da00cc
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-07 16:03:45 -0500

    finish rendering

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,11 +1,19 @@
 function ShoppingList() {
-    return (
-        <>
-            <h1>Shopping List</h1>
-            <button onClick={clearPurchaseStatus}>Reset</button>
-            <button onClick={removeAllItems}>Clear</button>
-        </>
-    );
+	const clearPurchaseStatus = () => {
+		console.log('Click');
+	};
+
+	const removeAllItems = () => {
+		console.log('Click');
+	};
+
+	return (
+		<>
+			<h1>Shopping List</h1>
+			<button onClick={clearPurchaseStatus}>Reset</button>
+			<button onClick={removeAllItems}>Clear</button>
+		</>
+	);
 }
 
 export default ShoppingList;
