45186c9b4d655fa9d5a91125916474f488ac63d6
Avery Yang <averyyang2674@gmail.com>
2022-07-07 15:11:17 -0500

    create ItemForm, ItemList, ShoppingList

---
+++ src/components/ItemForm/ItemForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function ItemForm() {
+    //states
+    const [newItem, setNewItem] = useState('');
+    const [newQuantity, setNewQuantity] = useState('');
+    const [newUnit, setNewUnit] = useState('');
+
+    return (
+        <>
+            <h2>Add an Item</h2>
+            <form>
+                <label>Item:</label>
+                <input
+                    onChange={(event) => setNewItem(event.target.value)}
+                    value={newItem}
+                />
+                <label>Quantity:</label>
+                <input
+                    onChange={(event) => setNewQuantity(event.target.value)}
+                    value={newQuantity}
+                />
+                <label>Unit:</label>
+                <input
+                    onChange={(event) => setNewUnit(event.target.value)}
+                    value={newUnit}
+                />
+                <button type="submit">Save</button>
+            </form>
+        </>
+    );
+}
+
+export default ItemForm;
