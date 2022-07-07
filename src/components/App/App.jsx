e45800e3cadc653c8b68499c4addfb8630c88497
Avery Yang <averyyang2674@gmail.com>
2022-07-07 15:15:39 -0500

    import components into app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,80 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../Header/Header.jsx'
-import './App.css';
-
-
-
+import Header from '../Header/Header.jsx';
+import ItemForm from '../ItemForm/ItemForm.jsx';
+import ShoppingList from '../ShoppingList/ShoppingList.jsx';
+import ItemList from '../ItemList/ItemList.jsx';
 
 function App() {
-    const [itemList, setItemList] = useState([])
-    const [newItem, setNewItem] = useState('')
-    const [newQuantity, setNewQuantity] = useState('')
-    const [newUnit, setNewUnit] = useState('')
+    const [itemList, setItemList] = useState([]);
 
+    useEffect(() => {
+        getItems();
+    }, []);
 
-    // useEffect(() => {
-    //     getItems()
-    // }, [])
-
-    // const getItems = () => {
-    //     axios.get('/list')
-    //     .then(response => {
-    //         setItemList(response.data)
-    //     })
-    //     .catch(err => {
-    //         alert('error getting items');
-    //         console.log(err);
-    //     })
-    // }
-
-
-
+    const getItems = () => {
+        axios
+            .get('/list')
+            .then((response) => {
+                setItemList(response.data);
+            })
+            .catch((err) => {
+                alert('error getting items');
+                console.log(err);
+            });
+    };
 
     return (
         <div className="App">
             <Header />
-            <main>
-                <h2>Add an Item</h2>
-
-
-                <form>
-        <label>Item:</label>
-        <input 
-        onChange={ (event) => setNewItem(event.target.value) } 
-        value={newItem}
-        />
-        <label>Quantity:</label>
-        <input 
-        onChange={ (event) => setNewQuantity(event.target.value) } 
-        value={newQuantity}/>
-        <label>Unit:</label>
-        <input 
-        onChange={ (event) => setNewUnit(event.target.value) } 
-        value={newUnit}/>
-        <button type="submit">Save</button>
-    </form>
-
-    <h2>Item List</h2>
-    <table>
-        <thead>
-        <tr>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Unit</th>
-        </tr>
-        </thead>
-        <tbody>
-                <td>Apples</td>
-                <td>5</td>
-                <td>2lbs</td>
-                <button type="delete">Remove</button>
-                <button type="Edit">Buy</button>
-        </tbody>
-    </table>
-
-            </main>
+            <ItemForm />
+            <ShoppingList />
+            <ItemList items={itemList} />
         </div>
-        
     );
 }
 
