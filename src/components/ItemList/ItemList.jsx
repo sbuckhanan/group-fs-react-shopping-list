3cfcf57c3c45917700601859f7c0e37feae0a3be
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-07 15:53:14 -0500

    format

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,37 +1,37 @@
 function ItemList() {
-    return (
-        <>
-            <h2>Item List</h2>
-            <table>
-                <thead>
-                <tr>
-                            <th>Name</th>
-                            <th>Quantity</th>
-                            <th>Unit</th>
-                            <th>Remove</th>
-                            <th>Buy</th>
-                        </tr>
-                    </thead>
-                <tbody>
-                        {itemList.map((item) => { 
-                        return(
-                            <tr>
-                            <td>{item.n}</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.unit}</td>
-                            <td>
-                                <button type='delete'>Remove</button>
-                            </td>
-                            <td>
-                                <button type='Edit'>Buy</button>
-                            </td>
-                        </tr>
-                        )})}
-                </tbody>
-            </table>
-        </>
-    );
+	return (
+		<>
+			<h2>Item List</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Quantity</th>
+						<th>Unit</th>
+						<th>Remove</th>
+						<th>Buy</th>
+					</tr>
+				</thead>
+				<tbody>
+					{itemList.map((item) => {
+						return (
+							<tr>
+								<td>{item.n}</td>
+								<td>{item.quantity}</td>
+								<td>{item.unit}</td>
+								<td>
+									<button type='delete'>Remove</button>
+								</td>
+								<td>
+									<button type='Edit'>Buy</button>
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</>
+	);
 }
-
 
 export default ItemList;
