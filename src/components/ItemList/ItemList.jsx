f74a1ef0ecaa525575eb6aeefbff55e9303ffa84
Avery Yang <averyyang2674@gmail.com>
2022-07-08 11:50:30 -0500

    complete reset button

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,27 +1,27 @@
 import Item from '../Item/Item';
 
 function ItemList({ items, getItems }) {
-	return (
-		<>
-			<h2>Item List</h2>
-			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Quantity</th>
-						<th>Unit</th>
-						<th>Remove</th>
-						<th>Buy</th>
-					</tr>
-				</thead>
-				<tbody>
-					{items.map((item) => (
-						<Item key={item.id} item={item} getItems={getItems} />
-					))}
-				</tbody>
-			</table>
-		</>
-	);
+    return (
+        <>
+            <h2>Item List</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Unit</th>
+                        <th>Remove</th>
+                        <th>Buy</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item) => (
+                        <Item key={item.id} item={item} getItems={getItems} />
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 }
 
 export default ItemList;
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,19 +1,32 @@
-function ShoppingList() {
-	const clearPurchaseStatus = () => {
-		console.log('Click');
-	};
+import axios from 'axios';
 
-	const removeAllItems = () => {
-		console.log('Click');
-	};
+function ShoppingList({ getItems }) {
+    // The `Reset` button should clear the purchased status from all items, allowing the list to be re-used.
+    const reset = () => {
+        console.log('reset clicked!');
+        axios
+            .put(`/list/reset`)
+            .then((response) => {
+                console.log(response);
+                getItems();
+            })
+            .catch((error) => {
+                alert('Error with updating', error);
+            });
+    };
 
-	return (
-		<>
-			<h1>Shopping List</h1>
-			<button onClick={clearPurchaseStatus}>Reset</button>
-			<button onClick={removeAllItems}>Clear</button>
-		</>
-	);
+    // The `Clear` button should remove all items from the list, deleting them from the database.
+    const clear = () => {
+        console.log('clear clicked!');
+    };
+
+    return (
+        <>
+            <h1>Shopping List</h1>
+            <button onClick={reset}>Reset</button>
+            <button onClick={clear}>Clear</button>
+        </>
+    );
 }
 
 export default ShoppingList;
