dc13cc797fb108ae2ba55eeba9f60b98f071361d
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-08 10:55:38 -0500

    fix components

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -16,34 +16,52 @@ function Item({ item, getItems }) {
 			});
 	};
 
-	// console.log(item);
+	const handleDelete = (event) => {
+		console.log('Click Delete');
+		const id = event.target.getAttribute('data-id');
+		console.log(id);
+		axios
+			.delete(`/list/${id}`)
+			.then((response) => {
+				console.log(response);
+				getItems();
+			})
+			.catch((error) => {
+				alert('Error with updating', error);
+			});
+	};
+
 	if (item.purchased) {
 		return (
-			<tr>
-				<td>{item.name}</td>
-				<td>{item.quantity}</td>
-				<td>{item.unit}</td>
-				<td>
-					<button type='delete'>Remove</button>
-				</td>
-				<td>PURCHASED</td>
-			</tr>
+			<>
+				<tr>
+					<td>{item.name}</td>
+					<td>{item.quantity}</td>
+					<td>{item.unit}</td>
+					<td>PURCHASED</td>
+					<td>PURCHASED</td>
+				</tr>
+			</>
 		);
 	} else {
 		return (
-			<tr key={item.id}>
-				<td>{item.name}</td>
-				<td>{item.quantity}</td>
-				<td>{item.unit}</td>
-				<td>
-					<button type='delete'>Remove</button>
-				</td>
-				<td>
-					<button type='Edit' onClick={handleBuy} data-id={item.id}>
-						Buy
-					</button>
-				</td>
-			</tr>
+			<>
+				<tr key={item.id}>
+					<td>{item.name}</td>
+					<td>{item.quantity}</td>
+					<td>{item.unit}</td>
+					<td>
+						<button type='submit' onClick={handleDelete} data-id={item.id}>
+							Remove
+						</button>
+					</td>
+					<td>
+						<button type='Edit' onClick={handleBuy} data-id={item.id}>
+							Buy
+						</button>
+					</td>
+				</tr>
+			</>
 		);
 	}
 }
